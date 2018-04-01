5655a691b7b05687e34fb09f5788529f82243de6
Yamichan <teta.pdu@gmail.com>
2018-04-01 18:26:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = new express();
+var express = require('express');
+var app = express();
+var path = require("path");
 
-app.get('/', function(request, response){
-    response.sendfile('./public/ImagesGallery/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/ImagesGallery/index.html'));
 });
