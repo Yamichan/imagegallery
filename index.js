a79c204371479d340cb945a1556ef7a2e4778da6
Yamichan <teta.pdu@gmail.com>
2018-06-14 23:26:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public/ImagesGallery'));
+app.get( '/', ( req: any, res: any ) => {
+    res.sendFile( path.resolve( __dirname, '/public/ImagesGallery/index.html' ) );
+} );
+export = app;
