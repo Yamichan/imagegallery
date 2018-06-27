68061c21ebbfe04e42593e7ebe09a0ca49265a4f
Yamichan <teta.pdu@gmail.com>
2018-06-27 17:35:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, '/public/ImagesGallery/index.html'));
 });
 
+app.get('/info', function (req, res) {
+    res.status( 200 ).send( { 'Hello I am Akatsuki!' } );
+});
+
 app.listen(8080, function () {
     console.log('This express angular app is listening on port:' + 8888);
 });
